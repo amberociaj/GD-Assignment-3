edc187b58abadca61425d1289b451a5b2a4ced4d
Musawenkosi <2127558@students.wits.ac.za>
2022-10-25 03:09:27 +0200

    Change weapons

--- Long Term Game/Assets/Scripts/Musa Scripts/SwordScript2.cs
+++ Long Term Game/Assets/Scripts/Musa Scripts/SwordScript2.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwordScript : MonoBehaviour
+public class SwordScript2 : MonoBehaviour
 {
-    [SerializeField]
-    float damageEnemy = 10f;
-
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            EnemyHealth enemyHealthScript = collision.transform.GetComponent<EnemyHealth>();
-            enemyHealthScript.DeductHealth(damageEnemy);
             Debug.Log("Hit");
         }
     }
